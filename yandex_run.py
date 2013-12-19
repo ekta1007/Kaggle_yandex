faf49eb85aca179ce1a7310dca49f0e6f8aed24c
Ekta Grover <ekta1007@gmail.com>
2013-12-19 11:51:07 +0530

    Update yandex_run.py

--- yandex_run.py
+++ yandex_run.py
@@ -1,4 +1,5 @@
 #executing & running the file for the session objects parsed
+# rough notes will path things up after the project is complete
 
 import gzip, yandex_parse
 f = gzip.open('train.gz', 'rb') #assuming train.gz is located in same base location
@@ -10,3 +11,10 @@ w = csv.writer(open("/yandex/output.csv", "w"))
 for x in range(0,len(sessions)):
   for key, val in sessions[x].items():
     w.writerow([key, val])
+  
+# reading part of the gzip file 
+import gzip
+f = gzip.GzipFile(fileobj=open('C:/Users/Ekta.Grover/Desktop/Downloads/yandex/train.gz', 'rb'))
+data = f.read(4000)
+print data
+print "done"
