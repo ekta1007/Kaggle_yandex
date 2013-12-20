f29cd4a68261fdafac33b60906f12da941908e98
Ekta Grover <ekta1007@gmail.com>
2013-12-20 23:24:07 +0530

    Update yandex_run.py

--- yandex_run.py
+++ yandex_run.py
@@ -64,4 +64,21 @@ print "Total userid records (with session metadata) in train file %d " %len(user
 print "Total unique userids records in train file %d" %len(set(userid))  # doing a set to just get the unique set of users in the userid list
 
 
+
+# 20-12-2013
+
+#creating a sampled file from the logs(ideally this should go on the parsed data so that we are sure that there's only one line (and hence no assciations between the lines)- in which case, one MUST sample all the records for THAT session, to have the COMPLETE picture
+import gzip
+from itertools import islice
+N = 20
+infile = gzip.GzipFile(fileobj=open('C:/Users/Ekta.Grover/Desktop/Downloads/yandex/train.gz', 'rb'))
+next_n_lines = islice(infile, N)
+i=1
+with open('C:/Users/Ekta.Grover/Desktop/train._logs.txt', 'w') as f:
+        for lines in next_n_lines:
+             f.write(lines)
+file_handle=open('C:/Users/Ekta.Grover/Desktop/train._logs.txt', 'rb')
+SAMPLE_COUNT=10
+reservoir_sampling(file_handle,SAMPLE_COUNT) # Store the sampled file
+
           
